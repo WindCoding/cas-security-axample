fd3c6b5ecbe4da27a676820ed78bb017ebd06eda
leaf <leaf@gmail.com>
2019-01-05 09:40:10 +0800

    activeMQ example

---
+++ springjms-producer/src/test/java/com/leaf/test/TestQueue.java
@@ -0,0 +1,23 @@
+package com.leaf.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.leaf.demo.QueueProducer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath*:applicationContext-jms-producer.xml")
+public class TestQueue {
+
+	@Autowired
+	private  QueueProducer queueProducer;
+	
+	@Test
+	public void testSend(){
+		queueProducer.sendTextMessage("spring JMS 点对点");
+	}
+	
+}
